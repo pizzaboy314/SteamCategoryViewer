1607c0617f8f050813099345fb8f5e9215b83d4a
Bryan Spahr <bspahr314@gmail.com>
2015-04-27 22:12:21 -0500

    now it parses the names of games using SteamGame objects, which in turn parse their tags from the store

--- src/Worker.java
+++ src/Worker.java
@@ -27,7 +27,7 @@ import javax.swing.filechooser.FileNameExtensionFilter;
 
 public class Worker {
 	
-	public static ArrayList<String> user1games;
+	public static ArrayList<SteamGame> usergames;
 
 	private static JFrame resultFrame;
 	private static JTextArea resultText;
@@ -38,7 +38,7 @@ public class Worker {
 	public static void main(String[] args) {
 		resultWindow();
 
-		user1games = new ArrayList<String>();
+		usergames = new ArrayList<SteamGame>();
 
 		String userName1 = null;
 		
@@ -60,12 +60,12 @@ public class Worker {
 
 		String user1URL = comURL + userName1 + endURL;
 
-		user1games = parseHTML(user1URL);
+		usergames = parseHTML(user1URL);
 
 
 		StringBuilder sb = new StringBuilder();
-		for (String s : user1games) {
-			sb.append(s + "\n");
+		for (SteamGame game : usergames) {
+			sb.append(game.getName() + "\n");
 		}
 
 		resultString = sb.toString();
@@ -77,64 +77,71 @@ public class Worker {
 
 	}
 
-	public static ArrayList<String> parseHTML(String input) {
-		ArrayList<String> games = new ArrayList<String>();
+	public static ArrayList<SteamGame> parseHTML(String input) {
+		ArrayList<SteamGame> games = new ArrayList<SteamGame>();
 		String url = input;
 
-		while (url != null) {
-			String nextURL = null;
+		try {
+			URL source = null;
+			boolean valid = true;
 			try {
-				URL source = null;
-				boolean valid = true;
+				source = new URL(url);
+			} catch (MalformedURLException e) {
+				valid = false;
+			}
+			while (valid == false) {
+				valid = true;
+				url = (String) JOptionPane.showInputDialog(null, "Malformed URL format. Are you sure you copied the entire URL?\n" + "Try again:",
+						"Provide URL", JOptionPane.PLAIN_MESSAGE, null, null, null);
 				try {
 					source = new URL(url);
 				} catch (MalformedURLException e) {
 					valid = false;
 				}
-				while (valid == false) {
-					valid = true;
-					url = (String) JOptionPane.showInputDialog(null,
-							"Malformed URL format. Are you sure you copied the entire URL?\n" + "Try again:", "Provide URL",
-							JOptionPane.PLAIN_MESSAGE, null, null, null);
-					try {
-						source = new URL(url);
-					} catch (MalformedURLException e) {
-						valid = false;
-					}
-				}
-
-				BufferedReader in = new BufferedReader(new InputStreamReader(source.openStream()));
-
-				String inputLine = in.readLine();
-				while (inputLine != null) {
-					// System.out.println(inputLine);
-					String[] arr;
-					if (inputLine.contains("rgGames")) {
-						arr = inputLine.split("name");
-						for (String line : arr) {
-							if (!line.contains("var rgGames") && line.contains("logo")) {
-								String game = line.substring(3);
-								game = game.substring(0, game.indexOf("\",\"logo\":"));
-								game = game.replace("\\u00ae", "");
-								game = game.replace("\\u221e", "");
-								game = game.replace("\\u2122", "");
-
-								games.add(game);
-
-								// System.out.println(game);
-							}
-						}
-						// System.out.println();
-					}
-					inputLine = in.readLine();
-				}
-
-				in.close();
-
-			} catch (Exception e) {
-				e.printStackTrace();
 			}
-			url = nextURL;
+
+			BufferedReader in = new BufferedReader(new InputStreamReader(source.openStream()));
+
+			String inputLine = in.readLine();
+			while (inputLine != null) {
+				// System.out.println(inputLine);
+				if (inputLine.contains("rgGames")) {
+					String s = inputLine.replace("var rgGames = [{", "").replace("}];", "").trim();
+					String[] apps = s.split("\\},\\{");
+					for (String app : apps) {
+						SteamGame game;
+						String gameTitle = null;
+						Integer gameID = null;
+
+						String[] params = app.split(",");
+						for (String param : params) {
+
+							String[] vals = param.split("\":");
+							if (vals[0].contains("name") && !vals[0].contains("friendly")) {
+								gameTitle = vals[1].replace("\"", "").replace("\\u00ae", "").replace("\\u221e", "").replace("\\u2122", "");
+							}
+							if (vals[0].contains("appid")) {
+								gameID = Integer.parseInt(vals[1].replace("\"", ""));
+							}
+							if (gameTitle != null && gameID != null) {
+								game = new SteamGame(gameTitle, gameID);
+								if (!game.isDlc()) {
+									games.add(game);
+								}
+								break;
+							}
+
+							System.out.println(param);
+						}
+					}
+				}
+				inputLine = in.readLine();
+			}
+
+			in.close();
+
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
 
 		return games;
