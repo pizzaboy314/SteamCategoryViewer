8387db0489b24b6fd8993fc738ddc090d6ce7a9d
Bryan Spahr <bspahr314@gmail.com>
2015-04-28 15:48:05 -0500

    fixed bug by adding birthdate cookie

--- src/SteamGame.java
+++ src/SteamGame.java
@@ -1,11 +1,8 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.net.MalformedURLException;
 import java.net.URL;
+import java.net.URLConnection;
 import java.util.ArrayList;
-
-import javax.swing.JOptionPane;
-
 
 public class SteamGame {
 
@@ -24,27 +21,19 @@ public class SteamGame {
 	public void parseCategories() {
 		tags = new ArrayList<String>();
 		String url = "http://store.steampowered.com/app/" + appid;
-
+		
 		try {
 			URL source = null;
-			boolean valid = true;
+			URLConnection conn = null;
 			try {
 				source = new URL(url);
-			} catch (MalformedURLException e) {
-				valid = false;
-			}
-			while (valid == false) {
-				valid = true;
-				url = (String) JOptionPane
-						.showInputDialog(null, "Malformed URL format. ", "Provide URL", JOptionPane.PLAIN_MESSAGE, null, null, null);
-				try {
-					source = new URL(url);
-				} catch (MalformedURLException e) {
-					valid = false;
-				}
+				conn = source.openConnection();
+				conn.setRequestProperty("Cookie", "birthtime=631224124");
+			} catch (Exception e) {
+				e.printStackTrace();
 			}
 
-			BufferedReader in = new BufferedReader(new InputStreamReader(source.openStream()));
+			BufferedReader in = new BufferedReader(new InputStreamReader(conn.getInputStream()));
 
 			String inputLine = in.readLine();
 			while (inputLine != null) {
